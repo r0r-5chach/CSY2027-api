7e8c059ee2f0d4168e55b4bd6a061a77ad3445d6
r0r-5chach <joshualukekillen@gmail.com>
2023-03-04 16:40:13 +0000

    added javadoc to entrypoint

--- src/main/java/com/r0r5chach/App.java
+++ src/main/java/com/r0r5chach/App.java
@@ -9,27 +9,41 @@ import static spark.Spark.put;
 import com.r0r5chach.services.AuthService;
 import com.r0r5chach.services.DBService;;
 
+/**
+ * Entrypoint class for the API. This sets up the paths and their references to methods in the code
+ * 
+ * @author r0r-5chach
+ */
 public class App 
 {
+    /**
+     * Main method for the API
+     * @param args arguments to be passed to the program
+     */
     public static void main( String[] args )
     {
+        //Route domain.com/db/
         path("/db", () -> {
+            //Route domain.com/db/one/
             path("/one", () -> {
                 //GET domain.com/db/?token=asd&collection=asd
-                get("/", DBService::getOne); //get item
+                get("/", DBService::getOne); //Get Document
+                
                 //POST domain.com/db/?token=asd&collection=asd
-                post("/", DBService::postOne); //make item  
+                post("/", DBService::postOne); //Make Document 
+                
                 //PUT domain.com/db/?token=asd&collection=asd
-                put("/", DBService::putOne); //update item
+                put("/", DBService::putOne); //Update Document
+                
                 //DELETE domain.com/db/?token=asd&collection=asd
-                delete("/", DBService::deleteOne); //delete item
+                delete("/", DBService::deleteOne); //Delete Document
             });
             
         });
-
+        //Route domain.com/auth/
         path("/auth", () -> {
             //POST domain.com/auth/?username=asd&password=asd
-            post("/", AuthService::post);
+            post("/", AuthService::post); //Request API token
         });
     }
 }
