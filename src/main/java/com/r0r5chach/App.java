ceb59305fdf27e41a9d03b8a672d1a12d04ec75b
r0r-5chach <joshualukekillen@gmail.com>
2023-03-04 15:39:54 +0000

    updated paths. added auth service

--- src/main/java/com/r0r5chach/App.java
+++ src/main/java/com/r0r5chach/App.java
@@ -2,23 +2,34 @@ package com.r0r5chach;
 
 import static spark.Spark.delete;
 import static spark.Spark.get;
-import static spark.Spark.options;
 import static spark.Spark.path;
 import static spark.Spark.post;
 import static spark.Spark.put;
 
-import com.r0r5chach.services.DBService;
+import com.r0r5chach.services.AuthService;
+import com.r0r5chach.services.DBService;;
 
 public class App 
 {
     public static void main( String[] args )
     {
         path("/db", () -> {
-            get("/", DBService::get);
-            post("/", DBService::post);           
-            put("/", DBService::put);
-            options("/", DBService::options);
-            delete("/", DBService::delete);
+            path("/one", () -> {
+                //GET domain.com/db/?token=asd&collection=asd
+                get("/", DBService::getOne); //get item
+                //POST domain.com/db/?token=asd&collection=asd
+                post("/", DBService::postOne); //make item  
+                //PUT domain.com/db/?token=asd&collection=asd
+                put("/", DBService::putOne); //update item
+                //DELETE domain.com/db/?token=asd&collection=asd
+                delete("/", DBService::deleteOne); //delete item
+            });
+            
+        });
+
+        path("/auth", () -> {
+            //POST domain.com/auth/?username=asd&password=asd
+            post("/", AuthService::post);
         });
     }
 }
---
+++ src/main/java/com/r0r5chach/services/AuthService.java
@@ -0,0 +1,10 @@
+package com.r0r5chach.services;
+
+import spark.Request;
+import spark.Response;
+
+public class AuthService {
+    public static boolean post(Request req, Response res) {
+        return false;
+    }
+}
