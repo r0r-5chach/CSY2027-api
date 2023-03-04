2ef9bba5eeeb27222ef0cc301f6f5fd9c6cfc56b
r0r-5chach <joshualukekillen@gmail.com>
2023-03-04 16:23:44 +0000

    Fixed put function

--- src/main/java/com/r0r5chach/services/DBService.java
+++ src/main/java/com/r0r5chach/services/DBService.java
@@ -13,6 +13,7 @@ import spark.Response;
 
 /**
  * Class that handles communicating to the database
+ * 
  * @author r0r-5chach
  */
 public class DBService {
@@ -20,8 +21,10 @@ public class DBService {
      * Static constant creating a client to access the database
      */
     private static final MongoDatabase client = MongoClients.create().getDatabase("woodlands");
+
     /**
      * Method that handles a GET request on route /db/one/
+     * 
      * @param req the request from the user
      * @param res the response to be sent to the user
      * @return A string containing the response in JSON format
@@ -30,36 +33,42 @@ public class DBService {
         res.type("application/json");
         Document query = parse(req.body());
         MongoCollection<Document> col = client.getCollection(req.queryMap().get("collection").value());
+        
         if (col.countDocuments(query) > 0) {
             res.status(200);
             return col.find(query).first().toJson();
-        }
+        } 
         else {
             res.status(500);
             return "{\"response\":\"No Documents found\"}";
         }
     }
+
     /**
      * Method that handles a POST request to route /db/one/
+     * 
      * @param req the request from the user
      * @param res the response to be sent to the user
      * @return The appropriate response code (201 Created; 500 Internal error)
      */
     public static int postOne(Request req, Response res) {
         Document item = parse(req.body());
+
         try {
             client.getCollection(req.queryMap().get("collection").value()).insertOne(item);
-        }
+        } 
         catch (Exception e) {
             res.status(500);
             return 500;
         }
-        
+
         res.status(201);
         return 201;
     }
+
     /**
      * Method that handles a PUT request to route /db/one/
+     * 
      * @param req the request from the user
      * @param res the response to be sent to the user
      * @return The appropriate response code (200 OK; 500 Internal error)
@@ -67,10 +76,11 @@ public class DBService {
     public static int putOne(Request req, Response res) {
         Document request = parse(req.body());
         Document query = parse(request.getString("query"));
-        Document update = parse(request.getString("update"));
+        Document update = new Document().append("$set", parse(request.getString("update")));
+
         try {
             client.getCollection(req.queryMap().get("collection").value()).updateOne(query, update);
-        }
+        } 
         catch (Exception e) {
             res.status(500);
             return 500;
@@ -79,8 +89,10 @@ public class DBService {
         res.status(200);
         return 200;
     }
+
     /**
      * Method that handles a DELETE request to route /db/one/
+     * 
      * @param req The request from the user
      * @param res The response to be sent to the user
      * @return The appropriate response code (200 OK; 404 Not Found)
@@ -91,7 +103,7 @@ public class DBService {
         if (client.getCollection(req.queryMap().get("collection").value()).findOneAndDelete(query) != null) {
             res.status(200);
             return 200;
-        }
+        } 
         else {
             res.status(404);
             return 404;
