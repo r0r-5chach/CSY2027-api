cfd17ef1c807e759c8b584cf1285b83338e4a349
r0r-5chach <joshualukekillen@gmail.com>
2023-03-06 19:42:48 +0000

    added property file to store db name

--- src/main/java/com/r0r5chach/services/DBService.java
+++ src/main/java/com/r0r5chach/services/DBService.java
@@ -1,5 +1,8 @@
 package com.r0r5chach.services;
 
+import java.io.FileReader;
+import java.util.Properties;
+
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
@@ -15,10 +18,21 @@ public class DBService extends Service {
     /**
      * Static constant creating a client to access the database
      */
-    protected static final MongoDatabase client = MongoClients.create().getDatabase("woodlands");
+    protected static final MongoDatabase client = MongoClients.create().getDatabase(getDB());
 
     protected static String options(Request req, Response res) {
         //TODO: create options method for database service
         return "";
-    }   
+    } 
+    
+    private static String getDB() {
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileReader(DBService.class.getResource(".properties").toString()));
+        }
+        catch (Exception e) {
+            System.out.println(e); //TODO: Handle Error
+        }
+        return prop.getProperty("database");
+    }
 }
