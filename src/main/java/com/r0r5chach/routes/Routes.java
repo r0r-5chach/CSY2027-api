5a82a8df83b71892d5d37dad26037c48665a6969
r0r-5chach <joshualukekillen@gmail.com>
2023-05-02 17:20:39 +0100

    restful function

--- src/main/java/com/r0r5chach/routes/Routes.java
+++ src/main/java/com/r0r5chach/routes/Routes.java
@@ -5,12 +5,16 @@ import static com.r0r5chach.routes.StaffRoutes.staff;
 import static com.r0r5chach.routes.StudentRoutes.student;
 import static com.r0r5chach.routes.UsersRoutes.users;
 import static spark.Spark.before;
+import static spark.Spark.delete;
+import static spark.Spark.get;
 import static spark.Spark.options;
 import static spark.Spark.path;
 import static spark.Spark.post;
+import static spark.Spark.put;
 
 import com.r0r5chach.services.UsersService;
 import com.r0r5chach.services.generic.AuthService;
+import com.r0r5chach.services.generic.Service;
 
 public class Routes {
     
@@ -45,5 +49,13 @@ public class Routes {
             //OPTIONS /auth/
             options("/", (req,res) -> service.options(req,res)); //return available options for route
         });
+    }
+
+    public static void restful(Service service) {
+        get("/", (req,res) -> service.get(req, res));
+        post("/", (req,res) -> service.post(req, res));
+        put("/", (req,res) -> service.put(req, res));
+        delete("/", (req,res) -> service.delete(req, res));
+        options("/", (req,res) -> service.options(req, res)); //return available options for sub-routes
     }
 }
