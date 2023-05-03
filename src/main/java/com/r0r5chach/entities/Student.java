5aeb6e86e8af25c6bfbe954281cb7146de109fbf
r0r-5chach <joshualukekillen@gmail.com>
2023-05-03 13:09:26 +0100

    added address

--- src/main/java/com/r0r5chach/entities/Student.java
+++ src/main/java/com/r0r5chach/entities/Student.java
@@ -2,10 +2,12 @@ package com.r0r5chach.entities;
 
 import java.util.List;
 
+
 public class Student extends Profile {
     private List<Course> courses;
     private List<Qualification> entryQualifications;
     private Staff personalTutor;
+    private Address termAddress, nonTermAddress;
 
     public Student(Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile) {
         super(name, recordStatus, dormantReason, phoneNumber, email, profile);
@@ -14,30 +16,41 @@ public class Student extends Profile {
         super(id, name, recordStatus, dormantReason, phoneNumber, email, profile);
     }
 
-    public Student(Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, List<Qualification> entryQualifications) {
+    public Student(Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, Address nonTermAddress) {
         super(name, recordStatus, dormantReason, phoneNumber, email, profile);
-        this.entryQualifications = entryQualifications;
+        this.nonTermAddress = nonTermAddress;
     }
-    public Student(int id, Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, List<Qualification> entryQualifications) {
+    public Student(int id, Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, Address nonTermAddress) {
         super(id, name, recordStatus, dormantReason, phoneNumber, email, profile);
+        this.nonTermAddress = nonTermAddress;
+    }
+
+    public Student(Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, Address nonTermAddress, List<Qualification> entryQualifications) {
+        this(name, recordStatus, dormantReason, phoneNumber, email, profile, nonTermAddress);
+        this.entryQualifications = entryQualifications;
+    }
+    public Student(int id, Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, Address nonTermAddress, List<Qualification> entryQualifications) {
+        this(id, name, recordStatus, dormantReason, phoneNumber, email, profile, nonTermAddress);
         this.entryQualifications = entryQualifications;
     }
 
-    public Student(Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, List<Qualification> entryQualifications, List<Course> courses) {
-        this(name, recordStatus, dormantReason, phoneNumber, email, profile, entryQualifications);
+    public Student(Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, Address nonTermAddress, List<Qualification> entryQualifications, Address termAddress, List<Course> courses) {
+        this(name, recordStatus, dormantReason, phoneNumber, email, profile, nonTermAddress, entryQualifications);
+        this.termAddress = termAddress;
         this.courses = courses;
     }
-    public Student(int id, Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, List<Qualification> entryQualifications, List<Course> courses) {
-        this(id, name, recordStatus, dormantReason, phoneNumber, email, profile, entryQualifications);
+    public Student(int id, Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, Address nonTermAddress, List<Qualification> entryQualifications, Address termAddress, List<Course> courses) {
+        this(id, name, recordStatus, dormantReason, phoneNumber, email, profile, nonTermAddress);
+        this.termAddress = termAddress;
         this.courses = courses;
     }
 
-    public Student(Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, List<Qualification> entryQualifications, List<Course> courses, Staff personalTutor) {
-        this(name, recordStatus, dormantReason, phoneNumber, email, profile, entryQualifications, courses);
+    public Student(Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, Address nonTermAddress, List<Qualification> entryQualifications, Address termAddress, List<Course> courses, Staff personalTutor) {
+        this(name, recordStatus, dormantReason, phoneNumber, email, profile, nonTermAddress, entryQualifications, termAddress, courses);
         this.personalTutor = personalTutor;
     }
-    public Student(int id, Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, List<Qualification> entryQualifications, List<Course> courses, Staff personalTutor) {
-        this(id, name, recordStatus, dormantReason, phoneNumber, email, profile, entryQualifications, courses);
+    public Student(int id, Name name, Status recordStatus, Reason dormantReason, String phoneNumber, String email, User profile, Address nonTermAddress, List<Qualification> entryQualifications, Address termAddress, List<Course> courses, Staff personalTutor) {
+        this(id, name, recordStatus, dormantReason, phoneNumber, email, profile, nonTermAddress, entryQualifications, termAddress, courses);
         this.personalTutor = personalTutor;
     }
     
@@ -59,6 +72,16 @@ public class Student extends Profile {
     public void setPersonalTutor(Staff personalTutor) {
         this.personalTutor = personalTutor;
     }
-
-    
+    public Address getTermAddress() {
+        return termAddress;
+    }
+    public void setTermAddress(Address termAddress) {
+        this.termAddress = termAddress;
+    }
+    public Address getNonTermAddress() {
+        return nonTermAddress;
+    }
+    public void setNonTermAddress(Address nonTermAddress) {
+        this.nonTermAddress = nonTermAddress;
+    }
 }
