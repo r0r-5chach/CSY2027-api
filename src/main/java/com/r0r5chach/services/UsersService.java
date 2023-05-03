d5a988fce5b751a0316ee14ac7cb4f40fd5a1754
r0r-5chach <joshualukekillen@gmail.com>
2023-05-03 14:04:43 +0100

    added user auth

--- src/main/java/com/r0r5chach/services/UsersService.java
+++ src/main/java/com/r0r5chach/services/UsersService.java
@@ -1,30 +1,32 @@
 package com.r0r5chach.services;
 
 import static org.bson.Document.parse;
+import static spark.Spark.halt;
 
 import org.bson.Document;
 
+import com.mongodb.client.MongoCollection;
 import com.r0r5chach.services.generic.AuthService;
-import com.r0r5chach.services.generic.Service;
+import com.r0r5chach.services.generic.DBService;
 
 import spark.Request;
 import spark.Response;
 
-public class UsersService extends Service{
+public class UsersService extends DBService{
+    private static final MongoCollection<Document> col = client.getCollection("users");
     public static void auth(Request req, Response res) {
-        Document query = parse(req.body());
+        Document userQuery = parse(req.body());
+        Document tokenQuery = new Document().append("token", req.queryMap().get("token").value());
         switch(req.requestMethod()) {
             case "POST":
             case "DELETE":
                 AuthService.adminAuth(req, res);
                 break;
             default:
-                //TODO: if user is same as queried user then allow access
-
+                if (!col.find(userQuery).first().getInteger("id").equals(col.find(tokenQuery).first().getInteger("id"))) {
+                    halt(404, "Unauthorized");
+                }
                 break;
         }
-    }
-
-    public String get(Request req, Response res) { 
     }
 }
