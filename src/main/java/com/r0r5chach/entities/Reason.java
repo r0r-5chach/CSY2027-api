dbe32782cb0abe3ae57aa15ff721d91e6042d678
r0r-5chach <joshualukekillen@gmail.com>
2023-05-03 13:35:48 +0100

    dormancy reasons

---
+++ src/main/java/com/r0r5chach/entities/Reason.java
@@ -0,0 +1,14 @@
+package com.r0r5chach.entities;
+
+public class Reason {
+    public enum Staff {
+        RETIRED,
+        RESIGNED,
+        MISCONDUCT
+    }
+    public enum Student {
+        GRADUATED,
+        WITHDRAW,
+        TERMINATED
+    }
+}
